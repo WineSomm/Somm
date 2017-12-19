935e2481a177da2000293ad605c9319fef580a6e
PrestonWinstead <prestonwinstead@me.com>
2017-12-18 20:10:13 -0600

    Update list based on search

--- client/navbar.js
+++ client/navbar.js
@@ -1,9 +1,18 @@
 angular.module('app')
   .component('navbar', {
+    bindings: {
+      wineList: '=',
+    },
     templateUrl: 'navbar.html',
-    controller() {
+    controller($scope, $http) {
       this.onClick = (input) => {
-
+        console.log(input);
+        const replaced = input.split(' ').join('+');
+        $http.get(`https://quiniwine.com/api/pub/wineKeywordSearch/${replaced}`)
+          .then((res) => {
+            console.log(res);
+            this.wineList = res.data.items;
+          });
       };
     },
   });
