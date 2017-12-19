e5b7c37f517e0d41aeea06b30ff28271732f47ac
PrestonWinstead <prestonwinstead@me.com>
2017-12-19 12:14:41 -0600

    Add to varietal data

--- client/varietalData.js
+++ client/varietalData.js
@@ -1,3 +1,4 @@
 window.varietalData = {
-  'France-Chardonnay': 'Chardonnay from Burgundy typically has notes of white stone fruit (like a white peach), citrus, and melon. It also usually has creamy notes as a result of malolactic fermentation. It is usually oaked, giving notes of toast and vanilla',
+  Chardonnay: 'Chardonnay from Burgundy typically has notes of white stone fruit (like a white peach), citrus, and melon. It also usually has creamy notes as a result of malolactic fermentation. It is usually oaked, giving notes of toast and vanilla.',
+  Gamay: 'French Gamay, mostly grown in Beaujolais, is a light or medium bodied wine with high acidity and low tannin. It usually has notes of red fruit (like strawberry, raspberry, and cherry), and is set apart from other fruity reds by a spicy complexity (usually cinnamon or pepper). All but the best should be consumed young, as they do not have the tannic structure to age well.',
 };
