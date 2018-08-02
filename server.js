b629d7ea525b113b275003d66887727e6e202174
aturek14@gmail.com <aturek14@gmail.com>
2018-08-02 15:41:07 -0500

    successfully getting recipes from api via postman requests

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { json, urlencoded } = require('body-parser');
 const axios = require('axios');
 const session = require('express-session');
 const { DB_TOKEN } = require('./database-config');
-const { API_TOKEN } = require('./api-config');
+const { API_TOKEN, EDAMAM_TOKEN, EDAMAM_ID } = require('./api-config');
 const { MAPS_TOKEN } = require('./maps-config');
 require('dotenv').config();
 
@@ -169,8 +169,19 @@ app.post('/search', (req, res) => {
     });
 });
 
+app.post('/recipes', (req, res) => {
+  const query = req.body.mealPreference;
+  axios.get(`https://api.edamam.com/search?q=${query}&app_id=${EDAMAM_ID}&app_key=${EDAMAM_TOKEN}`)
+    .then((response) => {
+      res.status(200).send(response.data.hits);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(404);
+    });
+});
+
 app.post('/online', (req, res) => {
-  // res.sendStatus(200);
   res.redirect('/');
 })
 
