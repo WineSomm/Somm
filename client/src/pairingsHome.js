846207de3b156970be1cfece806ebbc3109cfcc6
aturek14@gmail.com <aturek14@gmail.com>
2018-08-02 16:54:03 -0500

    fixed spelling mistake

--- client/src/pairingsHome.js
+++ client/src/pairingsHome.js
@@ -1,13 +1,5 @@
 angular.module('app')
-    .controller('homeCtrl', function() {
-        // const ctrl = this;
-        // ctrl.view = 'form';
-        // ctrl.changeview = function(viewChoice) {
-        //     ctrl.view = viewChoice;
-        // };
-        // ctrl.winerecomendations;
-        // ctrl.mealrecomendations;
-    })
+    .controller('homeCtrl', function() {})
     .component('pairingshome', {
         templateUrl: '../templates/pairings-home.html',
         controller: 'homeCtrl'
