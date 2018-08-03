d9f691bb60e254b339ba7c49c319c23663f127be
Senai Ayalew <senaiayalew@gmail.com>
2018-08-03 10:05:26 -0500

    Made init function to make a call when you first render my page '/buy'

--- client/src/buy.js
+++ client/src/buy.js
@@ -4,8 +4,25 @@ angular.module('app')
     },
     controller($scope, $http) {
       this.results = '';
+      this.onint = (input) => {
+        input = 'Dom Perignon champagne';
+        const keyword = input.split(' ').join('%20');
+        $scope.search = '';
+        $http.post('/buy', {
+          wine: keyword,
+        })
+          .then((res) => {
+            // this.results = res.data;
+            this.results = res.data.findItemsByKeywordsResponse[0].searchResult[0].item;
+            // this.url = this.results.galleryURL[0];
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      };
+      this.onint();
       this.buyonline = (input) => {
-        console.log(input, 'input');
+        // console.log(input, 'input');
         const keyword = input.split(' ').join('%20');
         $scope.search = '';
         $http.post('/buy', {
